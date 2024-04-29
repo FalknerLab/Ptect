b15041abe9ed302a0991de6d398e36d064919c98
falknerlab <falknerlab@gmail.com>
2024-04-29 12:47:01 -0400

    started io code for all data

---
+++ territorytools/io.py
@@ -0,0 +1,9 @@
+
+# Definitions of territory run
+# One recording contained in one folder
+#   - top cam video (ends in _top.mp4)
+#   - top cam sleap h5 (ends in _sleap.h5)
+#   - thermal video (ends in _thermal.avi)
+#   - thermal sleap h5 (ends in _sleaptherm.h5)
+def import_territory_experiment():
+    return None
