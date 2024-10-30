208ccfb43b44496373a3e1dbf8d4cc18783a6e15
DaveAndHisMice <da9769@princeton.edu>
2024-10-30 11:59:03 -0400

    many new functions. prefs across time. design matrix. dist to walls. computing histograms

--- territorytools/behavior.py
+++ territorytools/behavior.py
@@ -1,5 +1,7 @@
+import matplotlib.pyplot as plt
 import numpy as np
-from urine import urine_across_time
+from scrap.urine_old import urine_across_time
+from scipy.stats import zscore
 import warnings
 
 # Disabling runtime warning for mean of empty slice which doesn't seem to relate to any issues
@@ -85,13 +87,33 @@ def get_head_direction(mouse_data, in_deg=False, trunk_ind=5, head_ind=0): #orig
 
 
 def compute_preferences(x, y, walls=None):
-    ter_id = np.zeros_like(x)
-    for i, (x0, y0) in enumerate(zip(x, y)):
-        ter_id[i] = xy_to_territory(x0, y0, walls=walls)
+    # ter_id = np.zeros_like(x)
+    # for i, (x0, y0) in enumerate(zip(x, y)):
+    #     ter_id[i] = xy_to_territory(x0, y0, walls=walls)
+    ter_id = xy_to_territory(x, y)
     ter, ter_cnts = np.unique(ter_id, return_counts=True)
     prefs = np.zeros(3)
     prefs[ter.astype(int)] = ter_cnts / sum(ter_cnts)
     return prefs, ter_id
+
+
+def prefs_across_time(x, y, samp_win=5400):
+    pref_acc = []
+    for i in range(len(x)-samp_win):
+        pref_acc.append(compute_preferences(x[i:i+samp_win], y[i:i+samp_win])[0])
+    return np.array(pref_acc)
+
+
+def vel_across_time(vel, ter_id, samp_win=5400):
+    mean_vel_self = []
+    mean_vel_other = []
+    mean_vel_novel = []
+    for i in range(len(vel)-samp_win):
+        this_vel = vel[i:i+samp_win]
+        this_ter = ter_id[i:i+samp_win]
+        for ind, acc in enumerate((mean_vel_self, mean_vel_other, mean_vel_novel)):
+            acc.append(np.nanmean(this_vel[this_ter == ind]))
+    return mean_vel_self, mean_vel_other, mean_vel_novel
 
 
 def xy_to_territory(x, y, walls=None):
@@ -101,7 +123,8 @@ def xy_to_territory(x, y, walls=None):
     ter_a = np.logical_or(t < walls[0], t > walls[1])
     ter_b = np.logical_and(t > walls[0], t < walls[2])
     ter_c = np.logical_and(t > walls[2], t < walls[1])
-    ter_id = np.argmax([ter_a, ter_b, ter_c])
+    # ter_id = np.argmax([ter_a, ter_b, ter_c])
+    ter_id = np.argmax(np.vstack([ter_a, ter_b, ter_c]), axis=0)
     return ter_id
 
 
@@ -144,30 +167,73 @@ def compute_over_spatial_bin(xpos, ypos, data, func, bins=20, range=None):
     return out_hist, xedges, yedges
 
 
-def make_design_matrix(run_data, md):
+def make_design_matrix(run_data, md, norm=None):
     dist_btw_mice, looking_ang = [], []
-    if md['block'] == '0':
+    if md['Territory']['block'] == '0':
         dist_btw_mice, looking_ang = get_diadic_behavior(run_data[0]['x_cm'], run_data[0]['y_cm'], run_data[0]['angle'],
                                                          run_data[1]['x_cm'], run_data[1]['y_cm'])
     design_mat = []
     for m, d in enumerate(run_data):
+        x = np.abs(d['x_cm'])
         ut = urine_across_time(d['urine_data'], len_s=len(d['x_cm'])/40)
-        ut[ut > 1000] = np.nan
-        # ut = (ut > 0).astype(int)
         vel = d['velocity'] * 40
-        b_feats = [d['x_cm'], d['y_cm'], vel, d['angle'], ut]
+        b_feats = [x, d['y_cm'], vel, d['angle'], ut]
         for ft in b_feats:
-            norm_f = (ft - np.nanmin(ft)) / (np.nanmax(ft) - np.nanmin(ft))
             norm_f = ft
+            if norm == '0-1':
+                norm_f = (ft - np.nanmin(ft)) / (np.nanmax(ft) - np.nanmin(ft))
+            elif norm == 'zscore':
+                norm_f = zscore(ft)
             design_mat.append(norm_f)
 
     if len(dist_btw_mice) > 0:
         fts = [dist_btw_mice, looking_ang]
         for f in fts:
-            norm_f = (f - np.nanmin(f)) / (np.nanmax(f) - np.nanmin(f))
             norm_f = f
+            if norm == '0-1':
+                norm_f = (f - np.nanmin(f)) / (np.nanmax(f) - np.nanmin(f))
+            elif norm == 'zscore':
+                norm_f = zscore(f)
             design_mat.append(norm_f)
 
-    design_mat = np.array(design_mat)
+    design_mat = np.array(design_mat).T
 
     return design_mat
+
+
+def prob_over_x(x_data, binary_data, bin_min, bin_max, bin_num=20):
+    bins = np.linspace(bin_min, bin_max, bin_num)
+    tot_binary = np.sum(binary_data)
+    probs = []
+    prior = []
+    for b0, b1 in zip(bins[:-1], bins[1:]):
+        this_bin = np.logical_and(x_data > b0, x_data < b1)
+        probs.append(np.sum(binary_data[this_bin]) / tot_binary)
+        prior.append(np.sum(this_bin)/len(x_data))
+    prior = 100*np.array(prior)
+    probs = 100*np.array(probs)
+    bin_left_edge = bins[:-1]
+    return probs, prior, bin_left_edge
+
+
+def time_near_walls(x_pos, y_pos, wall_angs, dist_thresh=2, rad=30.48):
+    percents = []
+    in_zone = []
+    for a in wall_angs:
+        dtw = dist_to_wall(x_pos, y_pos, a)
+        percents.append(100 * (sum(dtw < dist_thresh) / len(x_pos)))
+        in_zone.append(dtw < dist_thresh)
+    return percents, in_zone
+
+
+def dist_to_wall(x, y, wall_deg, rad=30.48, num_pts=1000):
+    xy_data = np.vstack((x, y)).T
+    dist_acc = []
+    wall_xs = np.linspace(0, rad * np.sin(np.radians(wall_deg)), num_pts)
+    wall_ys = np.linspace(0, rad * np.cos(np.radians(wall_deg)), num_pts)
+    wall_pts = np.vstack((wall_xs, wall_ys)).T
+    for xy in xy_data:
+        xy_vec = np.ones((len(wall_xs), 2)) * np.expand_dims(xy, 1).T
+        dists = np.sqrt(np.sum((xy_vec - wall_pts)**2, axis=1))
+        dist_acc.append(np.min(dists))
+    return np.array(dist_acc)
