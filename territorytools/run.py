d7632ca2163207fb29df47ad9a179ee752241bd3
DaveAndHisMice <da9769@princeton.edu>
2024-10-30 12:10:22 -0400

    skeleton for running ptect from command line

---
+++ territorytools/run.py
@@ -0,0 +1,15 @@
+import argparse
+from gui import PtectGUI
+from process import import_all_data
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='TerritoryTools',
+                                     description='Data processing library for mouse territorial behaviors',
+                                     epilog='See documentation at github.com/Ptect')
+    parser.add_argument('datafolder', help='Path to territory run data')
+    parser.add_argument('-o', '--outdir', help='Path for saving data')
+    parser.add_argument('-gui', action='store_true', help='Pass to enable GUI')
+    args = parser.parse_args()
+    if args.gui:
+        PtectGUI(data_folder=args.datafolder)
