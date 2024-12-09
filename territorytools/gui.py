e6f841ac7faa4bd1a515a4f84be032926e0f7996
DaveAndHisMice <da9769@princeton.edu>
2024-12-09 16:54:28 -0500

    Close to v1. Added velocity and urine raster, sleap data plotting, urine xy plots with time of mark and colorbar

--- territorytools/gui.py
+++ territorytools/gui.py
@@ -7,11 +7,14 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5 import QtWidgets
+from matplotlib.collections import PathCollection
+
 from territorytools.process import import_all_data, valid_dir, find_territory_files
 from territorytools.urine import Peetector
 from territorytools.ttclasses import MDcontroller
 from territorytools.plotting import add_territory_circle
 from territorytools.behavior import get_territory_data
+from territorytools.utils import intersect2d
 import matplotlib
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg
 import matplotlib.pyplot as plt
@@ -20,7 +23,10 @@ import matplotlib.pyplot as plt
 matplotlib.use('QT5Agg')
 
 # Plotting Globals
-MOUSE_COLORS = ((255, 0, 0), (0, 150, 255))
+MOUSE_COLORS_BGR = []
+MOUSE_COLORS_MPL = ('tab:blue', 'tab:orange')
+for c in MOUSE_COLORS_MPL:
+    MOUSE_COLORS_BGR.append(255*np.fliplr(np.array(matplotlib.colors.to_rgb(c))[None, :])[0])
 
 def get_data_dialog():
     dialog = QFileDialog()
@@ -30,17 +36,24 @@ def get_data_dialog():
 
 class PtectController:
     data_buf = []
+    hot_data = np.empty((0, 3))
+    cool_data = np.empty((0, 2))
     def __init__(self, data_folder: str = None):
         if data_folder is None:
             data_folder = get_data_dialog()
 
         self.frame_num = 0
+        self.control_hz = 0
         self.valid = valid_dir(data_folder)
         if self.valid:
             t_files = find_territory_files(data_folder)
-            self.metadata = MDcontroller(t_files['metadata.yaml'])
+            self.metadata = MDcontroller(t_files['ptmetadata.yml'])
+            self.op_hz = self.metadata.get_val('Territory/optical_hz')
+            self.t_offset_frames = self.metadata.get_val('Territory/thermal_offset')
+            self.therm_hz = self.metadata.get_val('Territory/thermal_hz')
+            self.control_hz = max(self.op_hz, self.therm_hz)
             self.optical_vid = cv2.VideoCapture(t_files['top.mp4'])
-            sleap_file = h5py.File(t_files['top.h5'], 'r')
+            sleap_file = h5py.File(t_files['fixedtop.h5'], 'r')
             self.sleap_data = sleap_file['tracks']
             self.optical_data = []
             for i in self.sleap_data:
@@ -66,13 +79,26 @@ class PtectController:
             start_ind = self.frame_num - time_win
             start_ind = max(start_ind, 0)
 
+        if which_data == 'therm_len':
+            return self.ptect.get_length()
+
+        if which_data == 'length':
+            return self.optical_vid.get(cv2.CAP_PROP_FRAME_COUNT)
+
         if which_data == 'thermal':
-            times = None
-            hot_marks = None
-            if len(self.data_buf[0]) > 0:
-                times = self.data_buf[0][:, 0]
-                hot_marks = self.data_buf[0][:, 1:]
-            return times, hot_marks
+            data_cop = np.copy(self.hot_data)
+            # data_cop[:, 0] = np.round(self.control_hz*(data_cop[:, 0] / self.therm_hz))
+            is_self = data_cop[:, 1] < 0
+            self_marks = data_cop[is_self, :]
+            other_marks = data_cop[~is_self, :]
+            return self.hot_data, (self_marks, other_marks)
+
+        if which_data == 'velocity':
+            xys = []
+            for d in self.optical_data:
+                xys.append(d[3])
+            return xys
+
         if which_data == 'optical':
             xys = []
             for d in self.optical_data:
@@ -82,13 +108,14 @@ class PtectController:
             return None
 
     def get_optical_frame(self):
-        self.optical_vid.set(cv2.CAP_PROP_POS_FRAMES, self.frame_num)
+        op_frame_num = round(self.frame_num * (self.op_hz / self.control_hz))
+        self.optical_vid.set(cv2.CAP_PROP_POS_FRAMES, op_frame_num)
         ret, frame = self.optical_vid.read()
         for ind, sd in enumerate(self.sleap_data):
-            not_nan = ~np.isnan(sd[0, :, self.frame_num])
-            good_pts = sd[:, not_nan, self.frame_num].astype(int)
+            not_nan = ~np.isnan(sd[0, :, op_frame_num])
+            good_pts = sd[:, not_nan, op_frame_num].astype(int)
             for x, y in zip(good_pts[0, :], good_pts[1, :]):
-                cv2.circle(frame, (x, y), 2, MOUSE_COLORS[ind], -1)
+                cv2.circle(frame, (x, y), 5, MOUSE_COLORS_BGR[ind], -1)
         return ret, frame
 
     def read_next_frame(self, *args):
@@ -99,15 +126,23 @@ class PtectController:
         c_im = np.empty((resize_h, resize_w, 3))
         ret, frame = self.get_optical_frame()
         out_frame = None
+        t_frame_num = round(self.frame_num * (self.therm_hz / self.control_hz)) + self.t_offset_frames
         if ret:
-            urine_data, out_frame = self.ptect.peetect_frames(start_frame=self.frame_num,
+            urine_data, out_frame = self.ptect.peetect_frames(start_frame=t_frame_num,
                                                             num_frames=1, return_frame=True)
             rs_raw = cv2.resize(frame, (resize_w // 2, resize_h))
             rs_urine = cv2.resize(out_frame, (resize_w // 2, resize_h))
             c_im = np.hstack((rs_raw, rs_urine))
-            self.data_buf = urine_data
+            self.update_data(urine_data)
         self.set_frame(self.frame_num + 1)
         return c_im.astype('uint8'), frame, out_frame
+
+    def update_data(self, data):
+        if len(data[0]) > 0:
+            this_d = data[0]
+            this_d[:, 0] = self.frame_num
+            new_marks, new_inds = intersect2d(self.hot_data[:, 1:], this_d[:, 1:], return_int=False)
+            self.hot_data = np.vstack((self.hot_data, this_d[new_inds, :]))
 
     def set_param(self, param, value):
         match param:
@@ -153,14 +188,19 @@ class PtectGUI(QWidget):
 
     def __init__(self, *args, data_folder: str = None, **kwargs):
         super().__init__(*args, **kwargs)
-        self.resize(1280, 1400)
+        res = self.screen().size()
+        self.resize(int(res.width()*0.8), int(res.height()*0.8))
         self.setWindowTitle('Ptect Preview GUI')
         icon_path = os.path.abspath('../resources/ptect_icon.png')
         self.setWindowIcon(QIcon(icon_path))
 
-        self.preview_frame_w = 1280
-        self.preview_frame_h = 480
+        self.preview_frame_w = 1960
+        self.preview_frame_h = 840
+
+        print('Importing Data...')
         self.control = PtectController(data_folder=data_folder)
+
+        print('Initializing GUI...')
         first_frames = self.control.read_next_frame(self.preview_frame_w, self.preview_frame_h)
         self.prev_im = first_frames[2]
 
@@ -173,7 +213,7 @@ class PtectGUI(QWidget):
                          QImage.Format_BGR888)
         self.prev_frame.setPixmap(QPixmap(q_im))
         self.prev_frame.setScaledContents(True)
-        self.layout.addWidget(self.prev_frame, 0, 0, 4, 3)
+        self.layout.addWidget(self.prev_frame, 0, 0, 3, 2)
 
         self.add_controls()
 
@@ -190,10 +230,10 @@ class PtectGUI(QWidget):
         params = ['heat_thresh', 'cool_thresh', 'time_thresh']
         disp_names = ['Heat Thesh', 'Cool Thresh', 'Check Ahead # of Frames']
         num_slides = len(params)
-        end_slides = 3+num_slides
-        for i, p, n in zip(range(3, end_slides), params, disp_names):
+        end_slides = 2+num_slides
+        for i, p, n in zip(range(2, end_slides), params, disp_names):
             slider = SlideInputer(p, label=n)
-            self.layout.addWidget(slider, 3, i, 1, 1)
+            self.layout.addWidget(slider, 2, i, 1, 1)
             self.thresh_controls.append(slider)
 
         play = QPushButton('Play')
@@ -217,16 +257,16 @@ class PtectGUI(QWidget):
         self.run_info = QLabel(self.control.get_info())
         info_box.addWidget(self.run_info)
         info_gb.setLayout(info_box)
-        self.layout.addWidget(info_gb, 3, end_slides+1, 1, 1)
+        self.layout.addWidget(info_gb, 2, end_slides+1, 1, 1)
 
         dz_but = QPushButton('Add Dead Zone')
         dz_but.clicked.connect(self.set_dz)
-        self.layout.addWidget(dz_but, 2, 4, 1, 1)
+        self.layout.addWidget(dz_but, 1, 3, 1, 1)
         self.dz_pt_box = QLineEdit()
         self.dz_pt_box.setValidator(QIntValidator(3, 15))
-        self.layout.addWidget(self.dz_pt_box, 2, 5, 1, 1)
+        self.layout.addWidget(self.dz_pt_box, 1, 4, 1, 1)
         pt_lab = QLabel('# of Points')
-        self.layout.addWidget(pt_lab, 2, 6, 1, 1)
+        self.layout.addWidget(pt_lab, 1, 5, 1, 1)
 
         set_frame = QCheckBox('Show Steps')
         def set_frame_type():
@@ -235,21 +275,45 @@ class PtectGUI(QWidget):
             else:
                 self.control.set_param('frame_type', 0)
         set_frame.clicked.connect(set_frame_type)
-        self.layout.addWidget(set_frame, 2, 3, 1, 1)
+        self.layout.addWidget(set_frame, 1, 2, 1, 1)
 
         self.mark_plotter = PlotWidget()
-        add_territory_circle(self.mark_plotter.gca())
-        self.layout.addWidget(self.mark_plotter, 4, 1, 1, 1)
+        mark_ax = self.mark_plotter.gca()
+        mark_ax.set_title('Detected Marks')
+        add_territory_circle(mark_ax)
+        self.mark_plotter.colorbar(0, self.control.get_data('therm_len'), label='Mark Time (frame)')
+        self.layout.addWidget(self.mark_plotter, 3, 1, 2, 1)
 
         self.xy_plotter = PlotWidget()
-        add_territory_circle(self.xy_plotter.gca())
-        self.layout.addWidget(self.xy_plotter, 4, 0, 1, 1)
+        xy_ax = self.xy_plotter.gca()
+        xy_ax.set_title('XY Position (cm)')
+        add_territory_circle(xy_ax)
+        xy_ax.plot([0,0], [0,0], c=MOUSE_COLORS_MPL[0], label='Self')
+        xy_ax.plot([0,0], [0,0], c=MOUSE_COLORS_MPL[1], label='Other')
+        xy_ax.legend()
+        self.layout.addWidget(self.xy_plotter, 3, 0, 2, 1)
+
+        self.vel_plotter = PlotWidget()
+        vel_ax = self.vel_plotter.gca()
+        vel_ax.set_xlim(0, self.control.get_data('length'))
+        vel_ax.set_title('Mouse Velocity (normalized)')
+        vels = self.control.get_data('velocity')
+        vel_ax.plot(vels[0]/max(vels[0]), c=MOUSE_COLORS_MPL[0], label='Self')
+        vel_ax.plot(1 + vels[1]/max(vels[1]), c=MOUSE_COLORS_MPL[1], label='Other')
+        vel_ax.legend()
+        self.layout.addWidget(self.vel_plotter, 3, 2, 1, 6)
+
+        self.raster_plotter = PlotWidget()
+        rast_ax = self.raster_plotter.gca()
+        rast_ax.set_xlim(0, self.control.get_data('length'))
+        rast_ax.set_title('Marking Raster')
+        self.layout.addWidget(self.raster_plotter, 4, 2, 1, 6)
 
         arena_data = self.control.get_metadata('arena')
         self.arena_controls = ArenaSelector('Arena Controls', arena_type=arena_data[1])
         self.arena_controls.set_values(arena_data)
         self.arena_controls.set_frame(self.control.test_frame)
-        self.layout.addWidget(self.arena_controls, 0, 3, 2, num_slides+3)
+        self.layout.addWidget(self.arena_controls, 0, 2, 1, num_slides+3)
 
     def set_dz(self):
         self.control.set_param('deadzone', self.dz_pt_box.text())
@@ -265,13 +329,30 @@ class PtectGUI(QWidget):
         self.control.set_param('arena', arena_params)
 
         if self.playing:
-            times, hot_marks = self.control.get_data('thermal')
-            # if times is not None:
-            #     self.plotter.plot(hot_marks[:, 0], hot_marks[:, 1], plot_style='scatter', c=times)
+            hot_marks, split_marks = self.control.get_data('thermal')
+            if hot_marks is not None:
+                self.mark_plotter.clear()
+                self.mark_plotter.plot(hot_marks[:, 1], hot_marks[:, 2], s=0.05,
+                                       plot_style='scatter', c=hot_marks[:, 0],
+                                        marker='.', vmin=0, vmax=self.control.get_data('length'))
+
+            self.raster_plotter.clear()
+            for ind, sm in enumerate(split_marks):
+                marks = np.unique(sm[:, 0])
+                self.raster_plotter.plot(np.vstack((marks, marks)), [ind, ind+1], c=MOUSE_COLORS_MPL[ind])
+            self.raster_plotter.plot([self.control.frame_num, self.control.frame_num], [0, 2], 'k--')
+            self.raster_plotter.gca().set_xlim(self.control.frame_num-400, self.control.frame_num+400)
+
             self.xy_plotter.clear()
             mice_xys = self.control.get_data('optical', time_win=40)
+
             for ind, xy in enumerate(mice_xys):
-                self.xy_plotter.plot(xy[0], xy[1], c=np.array(MOUSE_COLORS[ind])/255)
+                self.xy_plotter.plot(xy[0], xy[1], c=MOUSE_COLORS_MPL[ind])
+
+            self.vel_plotter.clear()
+            self.vel_plotter.plot([self.control.frame_num, self.control.frame_num], [0, 2], 'k--')
+            self.vel_plotter.gca().set_xlim(self.control.frame_num - 400, self.control.frame_num + 400)
+
             frames = self.control.read_next_frame(self.preview_frame_w, self.preview_frame_h)
             frame = frames[0]
             self.prev_im = frames[2]
@@ -442,7 +523,8 @@ class MplCanvas(FigureCanvasQTAgg):
 class PlotWidget(QWidget):
     def __init__(self, parent=None):
         QWidget.__init__(self, parent)
-        self.current_pobj = None
+        self.cmap = 'cool'
+        self.current_pobj = []
         self.canvas = MplCanvas()
         pyqt_grey = (240/255, 240/255, 240/255)
         self.canvas.fig.set_facecolor(pyqt_grey)
@@ -457,18 +539,29 @@ class PlotWidget(QWidget):
         return self.canvas.ax
 
     def clear(self):
-        if self.current_pobj is not None:
-            for pobj in self.current_pobj:
-                pobj.remove()
+        if len(self.current_pobj) > 0:
+            for pobj_list in self.current_pobj:
+                if type(pobj_list) is PathCollection:
+                    pobj_list.remove()
+                else:
+                    for pobj in pobj_list:
+                        pobj.remove()
+            self.current_pobj = []
 
     def plot(self, *args, plot_style=None, **kwargs):
         my_ax = self.gca()
         if plot_style is not None:
             if plot_style == 'scatter':
-                self.current_pobj = plt.scatter(args[0], args[1], **kwargs)
+                self.current_pobj.append(my_ax.scatter(args[0], args[1], cmap=self.cmap, **kwargs))
         else:
-            self.current_pobj = my_ax.plot(*args, **kwargs)
+            self.current_pobj.append(my_ax.plot(*args, **kwargs))
         self.canvas.draw()
+
+    def colorbar(self, min_val, max_val, label='', orient='vertical'):
+        norm = matplotlib.colors.Normalize(vmin=min_val, vmax=max_val)
+
+        self.canvas.fig.colorbar(matplotlib.cm.ScalarMappable(norm=norm, cmap=self.cmap),
+                     ax=self.gca(), orientation=orient, label=label)
 
 
 class PtectApp:
