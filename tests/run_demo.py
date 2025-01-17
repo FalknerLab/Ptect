b62bd736265c8c51700cacdc22f3d6c132aef6e1
DaveAndHisMice <da9769@princeton.edu>
2025-01-17 16:16:08 -0500

    minor changes

--- tests/run_demo.py
+++ tests/run_demo.py
@@ -18,9 +18,9 @@ if __name__ == '__main__':
     fold_link = 'https://drive.google.com/drive/folders/1e58QlTkZTtZICjvpynQGK6FA5y0IkVGT?usp=sharing'
     demo_path = get_demo_data(fold_link)
     # demo_path = 'D:\\ptect_dataset\\PPsync4_Resident_DAB014_Intruder_DAB019_Day_0_Orientation_IRN\\Block0'
-    # gui = PtectApp(data_folder=demo_path)
+    gui = PtectApp(data_folder=demo_path)
     # out_data = np.load(os.path.join(demo_path, 'demo_ptect_full.npz'))
     # hd = out_data['hot_data']
     # plt.scatter(hd[:, 1], hd[:, 2], c=hd[:, 0])
     # plt.show()
-    process_all_data(demo_path, skip_ptect=False, show_all=True)
+    # process_all_data(demo_path, skip_ptect=False, show_all=True)
